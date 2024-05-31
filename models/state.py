b943f3dba6f9e8ca9b8fb00d752d9f47c07c1f46
Fayrouz20 <aitabdelaalifayrouz01@gmail.com>
2024-05-31 15:04:57 -0700

    make changes in storage file

--- models/state.py
+++ models/state.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-"""This is the state class"""
+
+"""
+A module that defines the ORM class for State table
+"""
 import os
-import models
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-        cities = relationship between state and city tables.
     """
-
+    Defines attributes for State table
+    """
     __tablename__ = 'states'
+
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship(
-            'City', back_populates='state',
-            cascade='all, delete, delete-orphan')
-
+        cities = relationship('City', backref='state', cascade='all, delete')
     else:
-        name = ""
+        name = ''
 
         @property
         def cities(self):
-            """returns list of Cities and some relationships"""
-            cities_instances = []
-            cities_dict = models.storage.all(models.City)
-            for key, value in cities_dict.items():
-                if self.id == value.state_id:
-                    cities_instances.append(value)
-            return cities_instances
+            """
+            Returns (list): List of City instances
+            with state_id equals to the current State.id
+            """
+            from models.city import City
+            from models import storage
+            city_list = list(storage.all(City).values())
+            return [city for city in city_list if city.state_id == self.id]
